396ad9464223753eda8332a92fbc13f15c1a92aa
h598999 <89342804+h598999@users.noreply.github.com>
2023-05-29 02:33:10 +0200

    test

---
+++ sommerProsjekt/sommerProsjekt/src/main/java/com/sommerProsjekt/sommerProsjekt/SommerProsjektApplication.java
@@ -0,0 +1,13 @@
+package com.sommerProsjekt.sommerProsjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SommerProsjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SommerProsjektApplication.class, args);
+	}
+
+}
---
+++ sommerProsjekt/sommerProsjekt/src/test/java/com/sommerProsjekt/sommerProsjekt/SommerProsjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sommerProsjekt.sommerProsjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SommerProsjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
